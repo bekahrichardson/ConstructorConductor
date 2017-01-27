bb70b5f2fac5b54d1de1d62aad8f5e5f65b9ba07
Rebekah Richardson <bekahrichardson0422@gmail.com>
2017-01-27 13:45:16 -0700

    user questions in arrays

--- instanceArray.js
+++ instanceArray.js
@@ -1,41 +1,55 @@
 /*
-  Your 3 Users will be the following. 
+  Your 3 Users will be the following.
     0) Tyler, tylermcginnis33@gmail.com, 'iLoveJS'
     1) Cahlan, cahlan@devmounta.in, 'iLoveHashtags'
     2) Lenny, lenny@theLenster.com, 'iLoveLentilSoup'
 */
 
-var User = function(name, email, pw){
+var Users = function(name, email, password) {
   this.name = name;
   this.email = email;
-  this.pw = pw;
+  this.password = password;
 }
 
 //Create an Array called 'users' that will store all our instances of User.
 
-  //code here
+  var Users = [];
 
 
 //Now create and push into your users array 3 separate instances of User using the data from above in that exact order
 
-  //code here
+  Users.push(new User('Tyler', 'tylermcginnis33@gmail.com', 'iLoveJS'));
+  Users.push(new User('Cahlan', 'cahlan@devmounta.in', 'iLoveHashtags'));
+  Users.push(new User('Lenny', 'lenny@theLenster.com', 'iLoveLentilSoup'));
 
 console.log('Tyler\'s information is ');
 //Console.log all of Tylers information
 
-  //code here
+  for (var i = 0; i < Users.length; i++) {
+    if(Users[i]['name'] === 'Tyler') {
+      console.log(Users[i])
+      break;
+    }
+  };
 
 console.log('Lenny\'s information is ');
 //Now console.log all of Lennys information
 
-  //code here
+  for (var i = 0; i < Users.length; i++) {
+    if(Users[i]['name'] === 'Lenny') {
+      console.log(Users[i])
+      break;
+    }
+  };
 
 
 //Now create another instance of User using your own information and then add that to your users array.
 
-  //code here
+  Users.push(new User('Bekah', 'bekahrichardson@gmail.com', 'iLoveCoding'));
 
 console.log('All my users names are ');
-//Now loop through your users Array and console.log every users name. 
+//Now loop through your users Array and console.log every users name.
 
-  //code here
+  for (var i = 0; i < Users.length; i++) {
+    console.log(Users[i])
+  };
--- quizApp.js
+++ quizApp.js
@@ -15,22 +15,31 @@
 
 //Create a Question constructor that accepts title, answersArray, rightAnswer, and difficulty parameters
 
-  //code here
+  var Question = function(title, answersArray, rightAnswer, difficulty) {
+    this.title = title;
+    this.answersArray = answersArray;
+    this.rightAnswer = rightAnswer;
+    this.difficulty = difficulty;
+  }
 
 
 //Create a quizUsers Array which is going to hold all of our users.
 
-  //code here
+  var quizUsers = ["Bob", "Jim", "Hal"];
 
 
 //Let's say three people signed up for our service, create 3 instances of User and add each to the users Array
 
-  //code here
-
+  quizUsers.push(new User('Bob', 'Bob@gmail', 'BobLovesCookies',
+  0));
+  quizUsers.push(new User('Jim', 'Jim@yahoo', 'JimLovesHersheys',
+  0));
+  quizUsers.push(new User('Hal', 'Hal@hotmail', 'HalLovesDoughnuts',
+  0));
 
 //Create a questions Array which is going to hold all of our questions
 
-  //code here
+  var questions = ["question1", "question2", "question3"];
 
 
 //Now, let's say we wanted to create a quiz about JavaScript. Create three instances of Question which contain the following data
@@ -39,14 +48,24 @@
 //title: "T/F: In Javascript, == doesn't check 'type' but just the value - where === checks type and value"
 //Fill in the rest of the required data as you see appropriate.
 
-  //code here
+  questions.push(new Question('T/F: Inheritance is achieved in JavaScript through Prototypes?',
+['T', 'F'], 'T', 'M'));
+questions.push(new Question('T/F: JavaScript is just a scripting version of Java',
+['T', 'F'], 'F', 'E'));
+questions.push(new Question("T/F: In JavaScript, == doesn't check 'type' but just the value - where === checks type and value",
+['T', 'F'], 'T', 'H'));
 
 
 //Now push all of your instances of Question into the questions Array
 
-  //code here
+  //Already done.
 
 console.log('My users Array and my questions arrray are ...');
 //Now loop console.log your users array and your questions array and verify that they're both holding the right data.
 
-  //code here
+  for (var i = 0; i < Users.length; i++) {
+    console.log(Users[i]);
+  };
+  for (var i = 0; i < Questions.length; i++) {
+    console.log(Questions[i]);
+  };
